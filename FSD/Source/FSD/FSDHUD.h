751ba3f477b4c8e5474affa974acdbd1b0c60ebf
Matt Sanders <rancor_13104@yahoo.com>
2022-02-05 22:59:27 -0800

    Make small improvements to dummied classes

--- FSD/Source/FSD/FSDHUD.h
+++ FSD/Source/FSD/FSDHUD.h
@@ -6,6 +6,19 @@
 #include "GameFramework/HUD.h"
 #include "FSDHUD.generated.h"
 
+
+UENUM()
+enum class EHUDVisibilityReason : uint8
+{
+	Invalid = 0,
+	UserChoice = 1,
+	StandDown = 2,
+	MenuActive = 4,
+	Photography = 8,
+	EHUDVisibilityReason_MAX = 9
+};
+
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class FSD_API AFSDHUD : public AHUD
 {
@@ -16,16 +29,14 @@ public:
 	//UPROPERTY(BlueprintReadOnly) FMulticastInlineDelegate OnHUDVisibilityChanged;
 	//UPROPERTY(BlueprintReadOnly) char IsVisibleFlags;
 
-	//UFUNCTION(BlueprintCallable) bool ToggleHUDVisibility(enum class EHUDVisibilityReason reason) { return false; };
+	UFUNCTION(BlueprintCallable) bool ToggleHUDVisibility(EHUDVisibilityReason reason) { return false; };
 	UFUNCTION(BlueprintCallable) void ShowObjectives(bool InVisibility) {};
 	UFUNCTION(BlueprintCallable) void SetObjectivesVisible__DelegateSignature(bool InVisible, bool animate) {};
-	//UFUNCTION(BlueprintCallable) void SetHUDVisible(bool IsVisible, enum class EHUDVisibilityReason reason) {};
-	//UFUNCTION(BlueprintCallable) void RadarPointAdded(URadarPointComponent* radarPoint) {};
+	UFUNCTION(BlueprintCallable) void SetHUDVisible(bool IsVisible, EHUDVisibilityReason reason) {};
 	//UFUNCTION(BlueprintCallable) void PlayerSpawned(APlayerCharacter* Player) {};
 	UFUNCTION(BlueprintCallable) void OnVisibilityChanged() {};
-	//UFUNCTION(BlueprintCallable) bool IsHUDVisibleFlagSet(enum class EHUDVisibilityReason reason) { return false; };
+	UFUNCTION(BlueprintCallable) bool IsHUDVisibleFlagSet(EHUDVisibilityReason reason) { return false; };
 	UFUNCTION(BlueprintCallable) void HudVisibilityChanged__DelegateSignature(bool InHudVisible) {};
-	//UFUNCTION(BlueprintCallable) void HandleSeamlessTravel() {};
 	UFUNCTION(BlueprintCallable) bool GetHUDVisible() { return false; };
 	UFUNCTION(BlueprintCallable) AFSDHUD* GetFSDHUD(APlayerController* InPlayerController) { return NULL; };
 	//UFUNCTION(BlueprintCallable) void CameraDroneSpawned(APlayerCameraDrone* Drone) {};
--- FSD/Source/FSD/MissionGenerationManager.h
+++ FSD/Source/FSD/MissionGenerationManager.h
@@ -6,7 +6,7 @@
 #include "GeneratedMission.h"
 #include "MissionGenerationManager.generated.h"
 
-UCLASS() //ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+UCLASS()
 class UMissionGenerationManager : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
@@ -15,7 +15,7 @@ public:
 	//UPROPERTY(BlueprintReadOnly) TMap<int32_t, FGeneratedMissionGroup> AllMissionGroups;
 	//UPROPERTY(BlueprintReadOnly) bool ResetSelectedMission;
 
-	UFUNCTION(BlueprintCallable) TArray<UGeneratedMission*> GetMissions(int32 Seed) { return *new TArray<UGeneratedMission*>(); };
+	UFUNCTION(BlueprintCallable) TArray<UGeneratedMission*> GetMissions(int32 Seed) { return TArray<UGeneratedMission*>(); };
 	UFUNCTION(BlueprintCallable) UGeneratedMission* GetMissionFromSeeds(int32 GlobalSeed, int32 MissionSeed) { return NULL; };
-	UFUNCTION(BlueprintCallable) TArray<UGeneratedMission*> GetAvailableMissions() { return *new TArray<UGeneratedMission*>(); };
+	UFUNCTION(BlueprintCallable) TArray<UGeneratedMission*> GetAvailableMissions() { return TArray<UGeneratedMission*>(); };
 };
