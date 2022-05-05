ad84a37f8062ef8eaef22774cedc6af789825a8c
Matt Sanders <rancor_13104@yahoo.com>
2022-05-05 01:31:24 -0700

    Upgrade project and scripts to UE 4.27

--- FSD/Package/unpack.bat
+++ FSD/Package/unpack.bat
@@ -6,7 +6,7 @@ REM
 REM This script must be executed from the same directory it exists in, due to use of the "CD" var.
 REM
 
-SET PATH=%PATH%;C:\Games\UE_4.25\Engine\Binaries\Win64\
+SET PATH=%PATH%;C:\Games\UE_4.27\Engine\Binaries\Win64\
 
 UnrealPak.exe %1 -extract "%CD%\unpacked"
 
